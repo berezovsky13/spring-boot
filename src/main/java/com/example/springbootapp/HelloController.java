2b56e8e1af00c047ac2ca153520a3e6e48eddb3d
berezovsky13 <danibiri3@gmail.com>
2024-09-29 20:48:57 +0300

    adding other java files

--- src/main/java/com/example/springbootapp/HelloController.java
+++ src/main/java/com/example/springbootapp/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.hello_daniel;
+package com.example.springbootapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, Daniel";
+        return "Hello, Daniel!";
     }
 }
--- src/main/java/com/example/springbootapp/SpringBootAppApplication.java
+++ src/main/java/com/example/springbootapp/SpringBootAppApplication.java
@@ -1,11 +1,11 @@
-package com.example.hello_daniel;
+package com.example.springbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloDanielApplication {
+public class SpringBootAppApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HelloDanielApplication.class, args);
+        SpringApplication.run(SpringBootAppApplication.class, args);
     }
 }
